f1f6052850df9698a0864d74c4d7679412bc1e37
flores <flores@petalphile.com>
2011-10-26 15:55:59 -0700

    does not belong here

--- convert_existing_blog_to_wordpress.pl
+++ convert_existing_blog_to_wordpress.pl
@@ -16,7 +16,6 @@
 #
 ##########
 
-use strict;
 use DBI;
 
 
--- sum_httperf_results.sh
+++ sum_httperf_results.sh
@@ -1,5 +1,9 @@
 # really just a one-liner copied into .sh https://gist.github.com/1174722
 
+function distribute () {
+	for i in `seq 1 4`; do ssh root@dolphin$i.borderstylo.int " $* " & done
+}
+
 function sum_that_shit () { curl -s $* |perl -pi -e 's/div/\n/g' |perl -nl -e 'if ($_ =~ /Request rate: (.+) req/) { $sum+=$1; } elsif ($_ =~ /Errors: total (.+)?\s/) { $errors+=$1; } elsif ($_ =~ /Total: connections (.+)?\s/) { $conns+=$1; } elsif ($_ =~ /title>(.+)?\s&mdash/) { $title=$1; } END  { $success_percent = 100 * ($conns-$errors)/$conns; print "$title\n$sum requests/s\n$conns total connections\n$errors total errors\n$success_percent\% success\n\n"; }'; }
 
 
